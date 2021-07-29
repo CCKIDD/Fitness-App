2e7d0625f54118348bb03cb9ebcd635f8d6daac5
CCKIDD <c.chase.kidd@gmail.com>
2021-07-29 16:20:49 -0400

    initial commit

---
+++ Fresh and Fit/Fresh and Fit/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Fresh and Fit
+//
+//  Created by Christopher Kidd on 7/29/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
